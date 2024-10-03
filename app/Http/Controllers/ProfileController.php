d2f812be1857f388890aaf4dd245065f78c2ec4a
Kthalv <alvaradokathline@gmail.com>
2024-10-03 11:26:24 +0800

    First Commit

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Profile;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function getAllProfiles()    
+    {
+        $profiles = Profile::all();
+        return view('profiles', ['profiles' => $profiles]);
+    }
+    public function show ($id)
+    { 
+         $profile = Profile::find($id);
+
+        if (!$profile) {
+            return abort(404);
+        }
+        return view('show', compact('profile'));
+    }
+}
---
+++ resources/views/profiles.blade.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profiles Page</title>
+</head>
+<body>
+    <h1>PROFILES PAGE</h1>
+    
+    @foreach ($profiles as $profile)
+         <div> <b>ID:</b> {{ $profile->id }}</div>
+         <div> <b>name:</b> {{ $profile->name }}</div>
+         <div> <b>email:</b> {{ $profile->email }}</div>
+         <div> <b>birthdate:</b> {{ $profile->birthdate }}</div>
+         <div> <b>home_address:</b> {{ $profile->home_address }}</div>
+         <div> <b>contact_number:</b> {{ $profile->contact_number }}</div>
+         <hr>
+    @endforeach
+</body>
+</html>
